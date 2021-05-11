570f483bd47f9851ff4389880abb77b31ccc3ded
Gitthuma <george.githuma@gmail.com>
2021-05-11 22:46:54 +0300

    Draw the triangle inside drawTriangle function

--- code/main.js
+++ code/main.js
@@ -1,11 +1,22 @@
 /*Draw a triangle in a fixed position inside canvas using function drawTriangle*/
 
 /*Reference the canvas and the content of the canvas and store them in variables*/
+
 const canvas = document.querySelector('#myCanvas');
 const context = canvas.getContext('2d');
 
 /*Create function statement for drawTriangle*/
+
 function drawTriangle() {
+
+    // draw the triangle
+
+    context.beginPath();
+    context.moveTo(200, 100);
+    context.lineTo(170, 150);
+    context.lineTo(230, 150);
+    context.closePath();
+
 
 }
 
