207f1caa946ca7d6e34f6758a2a4e42d06dbf009
Gitthuma <george.githuma@gmail.com>
2021-05-11 22:34:44 +0300

    Starter drawing a tringle inside canvas by first referencing the canvas and the content of the canvas and storing them in variables

---
+++ code/main.js
@@ -0,0 +1,6 @@
+/*Draw a triangle in a fixed position inside canvas using function drawTriangle*/
+
+/*Reference the canvas and the content of the canvas and store them in variables*/
+const canvas = document.querySelector('#myCanvas');
+const context = canvas.getContext('2d');
+
